5144ae7b077416cd9e51d9e985acf536cd1352b4
Mehrdadrd1 <myemail>
2023-10-16 18:25:07 +0330

    html andd css added

---
+++ javaScript_Memory.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("myForm");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let workName = document.getElementById("wName");
+  let dateOfTodo = document.getElementById("dTodo");
+  let description = document.getElementById("descriptionTodo");
+});
